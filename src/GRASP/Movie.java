6e84fbd12282dd54ee23508439b1e9a73da7be6d
NewWisdom <wlgp2500@gmail.com>
2021-02-13 20:26:29 +0900

    05-03(2021.02.13): 다형성을 통해 분리하기, 변경으로 부터 보호하기

--- src/GRASP/Movie.java
+++ src/GRASP/Movie.java
@@ -1,12 +1,12 @@
 package GRASP;
 
-import Data.DiscountCondition;
 import Data.Money;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
@@ -15,6 +15,13 @@ public class Movie {
     private MovieType movieType;
     private Money discountAmount;
     private double discountPercent;
+
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = Arrays.asList(discountConditions);
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if (isDiscountable((screening))) {
@@ -29,28 +36,9 @@ public class Movie {
                 .anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case AMOUNT_DISCOUNT:
-                return calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-            case NONE_DISCOUNT:
-                return calculateNoneDiscountAmount();
-        }
+    abstract protected Money calculateDiscountAmount();
 
-        throw new IllegalStateException();
-    }
-
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
+    protected Money getFee() {
+        return fee;
     }
 }
